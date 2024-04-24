a17094fbb8249380524d4d3b946ce4d0b4b48412
chaos <940010004@qq.com>
2024-04-24 23:55:46 +0800

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,5 @@
 export const Home = () => {
 	return (
-		<div className=" bg-slate-200 font-bold from-neutral-600 font-3xl">
-			Home
-		</div>
+		<div className="font-3xl bg-slate-200 from-neutral-600 font-bold">Home</div>
 	);
 };
