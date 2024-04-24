5c26a8a662943e0d6d49b5cc3426a0bc6de43db2
chaos <940010004@qq.com>
2024-04-24 23:26:05 +0800

    fix: tailwind config error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-
 export const Home = () => {
-	return <div className="">Home</div>;
+	return (
+		<div className=" bg-slate-200 font-bold from-neutral-600 font-3xl">
+			Home
+		</div>
+	);
 };
