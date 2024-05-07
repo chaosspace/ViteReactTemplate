79a4158f170d821ee0685d5f74425a71bdeaa122
chaos <940010004@qq.com>
2024-05-07 15:21:23 +0800

    feat: global toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import router from "./routes";
 import { RouterProvider } from "react-router-dom";
-import { ErrorBoundary } from "./components";
+import { Toaster, useToast } from "./hooks";
 
 function App() {
+	const { toast } = useToast();
+
 	return (
-		<ErrorBoundary>
-			App
+		<>
+			<Toaster />
+			<div
+				onClick={() => {
+					toast({ type: "error", msg: "请求出错了，待会再试试吧" });
+				}}
+			>
+				App
+			</div>
 			<RouterProvider router={router} />
-		</ErrorBoundary>
+		</>
 	);
 }
 
