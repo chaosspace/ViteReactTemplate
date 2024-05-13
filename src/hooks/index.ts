be12d19bb702c73c17b99e94eedfe1e6af8e820a
chaos <940010004@qq.com>
2024-05-13 19:00:21 +0800

    feat: scrollprogress

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,44 @@
 export * from "./toast";
+import { MutableRefObject, useEffect, useState } from "react";
+
+type BasicTarget<T> = T | null | undefined;
+type Target =
+	| BasicTarget<HTMLElement | Window>
+	| MutableRefObject<BasicTarget<HTMLElement | Window>>;
+
+const useEventListener = (
+	eventName: keyof HTMLElementEventMap,
+	handler: (evt: Event) => void,
+	target: Target
+) => {
+	useEffect(() => {
+		if (!target || !("current" in target)) return;
+
+		const domElement = target.current;
+		if (domElement) {
+			domElement.addEventListener(eventName, handler);
+		}
+
+		return () => {
+			if (domElement) {
+				domElement.removeEventListener(eventName, handler);
+			}
+		};
+	}, [eventName, handler, target]);
+};
+
+export const useScrollProgress = <T extends HTMLElement>(
+	target?: MutableRefObject<T | null | undefined>
+) => {
+	const [progress, setProgress] = useState(0);
+
+	const handler = () => {
+		const domElement = target?.current ?? document.body;
+		const offset = domElement.scrollHeight - domElement.clientHeight;
+		setProgress(domElement.scrollTop / offset);
+	};
+
+	useEventListener("scroll", handler, target);
+
+	return progress;
+};
--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,3 +1,19 @@
+import { useScrollProgress } from "@/hooks";
+import { useRef } from "react";
+
 export const Test = () => {
-	return <div>Test</div>;
+	const ref = useRef<HTMLDivElement>(null);
+	const progress = useScrollProgress(ref);
+
+	return (
+		<div>
+			Test
+			<div ref={ref} style={{ height: "100px", overflow: "auto" }}>
+				<div style={{ height: "200px", backgroundColor: "azure" }}>
+					sjdfljaslk
+				</div>
+			</div>
+			{progress}
+		</div>
+	);
 };
