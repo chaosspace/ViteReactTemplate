3aa806d77d189a1cd66ae75ef3e5ce33aafbf38d
chaos <940010004@qq.com>
2024-08-05 21:08:24 +0800

    top muser slide list test

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,8 +1,19 @@
 import { useLongPress, useScrolledIndex, useScrollProgress } from "@/hooks";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { TestComp } from "@/components/TestComp";
 import { NestComp } from "@/components/NestComp";
 import { Link } from "react-router-dom";
+import styles from "./Test.module.css";
+
+const data = [
+	"pink",
+	"red",
+	"orange",
+	"yellow",
+	"greenyellow",
+	"aqua",
+	"black"
+];
 
 export const Test = () => {
 	const pressRef = useRef<HTMLDivElement>(null);
@@ -11,6 +22,22 @@ export const Test = () => {
 	const [isPressed] = useLongPress(pressRef);
 	const [index] = useScrolledIndex(scrollRef);
 	const [isOpen, setIsOpen] = useState(false);
+
+	const [active, setActive] = useState(0);
+	const transformedData = useMemo(() => [...data, ...data.slice(0, 3)], [data]);
+	const renderData = useMemo(
+		() =>
+			transformedData.slice(active % data.length, (active % data.length) + 4),
+		[active]
+	);
+
+	const prevIndex = useMemo(
+		() => (active - 1 + renderData.length) % renderData.length,
+		[active]
+	);
+
+	console.log(transformedData[prevIndex]);
+	console.log(transformedData[active]);
 
 	return (
 		<div>
@@ -76,6 +103,26 @@ export const Test = () => {
 						flexShrink: 0
 					}}
 				></div>
+			</div>
+			<div className={styles.slideList}>
+				{/* <div
+					className={styles.default}
+					style={{ zIndex: 99, backgroundColor: "white" }}
+				></div> */}
+				{renderData.map((item, index) => {
+					const len = data.length;
+					return (
+						<div
+							key={item}
+							className={styles.default}
+							style={{
+								backgroundColor: item,
+								zIndex: renderData.length - 1 !== index ? len - index : 0
+							}}
+							onClick={() => setActive(active + 1)}
+						></div>
+					);
+				})}
 			</div>
 		</div>
 	);
